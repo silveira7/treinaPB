422bbb6ccbc235d505d306e0815ec0e0259ba517
Gustavo Silveira <silveira@tuta.io>
2021-03-14 17:25:36 -0300

    Fix bug in VV tier generator
    Current modifications made VV tier generator independent of the position of characters in line (which was causing some bugs).

--- vv_tier.py
+++ vv_tier.py
@@ -12,7 +12,6 @@ os.chdir(folder)
 files = sorted(os.listdir(folder))
 
 for file in files:
-    print(file)
     if re.search('_merged.TextGrid', file):
 
         # Import TextGrid
@@ -28,7 +27,7 @@ for file in files:
                 lower_limit = tg.index(line)
             if re.search(r'item \[2]', line):
                 upper_limit = tg.index(line)
-
+        [print(x) for x in tg[upper_limit:]]
         # Store only the 1st tier's intervals in a new variable
         phone_tier = copy.deepcopy(tg[lower_limit:upper_limit])
 
@@ -37,40 +36,49 @@ for file in files:
         # Regex to identify characters representing consonantal sounds
         consonant = r'p|b|t|d|k|g|f|s|sh|v|z|zh|S|m|n|nh|r|R|l|lh|L'
 
-        one_consonant_begin = False
-        two_consonants_begin = False
-        line_ignore = 9
-
         # Remove ""
         for index, value in enumerate(phone_tier):
             if re.search(r'\t\t\t\ttext = ""\n', value):
                 del phone_tier[index-3:index+1]
                 pass
 
-        # Remove sil
+        # Remove sil except the first
         for index, value in enumerate(phone_tier):
-            if index != 9 and re.search(r'\t\t\t\ttext = "sil"\n', value):
+            if index != 9 and re.search(r'\"sil\"', value):
                 del phone_tier[index-3:index+5]
                 pass
 
-        if re.search(consonant, phone_tier[13][12:-2]):
+        # Check if the tier starts with one or two consonants
+        one_consonant_begin = False
+        two_consonants_begin = False
+        line_ignore = 9
+
+        text = re.compile(r'\"\w+\"')
+
+        first_segment = re.search(text, phone_tier[13]).group()[1:-1]
+        second_segment = re.search(text, phone_tier[17]).group()[1:-1]
+
+        if re.search(consonant, first_segment):
             one_consonant_begin = True
             line_ignore = 13
-            if re.search(consonant, phone_tier[17][12:-2]):
+            if re.search(consonant, second_segment):
                 two_consonants_begin = True
                 line_ignore = 17
 
-        # Get the xmax of the first interval (silence)
+        time = re.compile(r'\d+\.\d+')
+
+        # Get the modified xmax of the first interval (silence)
         if one_consonant_begin:
             # xmax is equal to xmin of fourth interval (skip the two first consonants)
             if two_consonants_begin:
-                first_max = phone_tier[19][11:-1]
+                first_max = re.search(time, phone_tier[19]).group()
             # xmax is equal to xmin of third interval (skip the first consonant)
             else:
-                first_max = phone_tier[15][11:-1]
+                first_max = re.search(time, phone_tier[15]).group()
         else:
-            first_max = phone_tier[8][11:-1]
+            first_max = re.search(time, phone_tier[8]).group()
 
+        # Add the first sil interval to vv_tier
         vv_tier = vv_tier + ['\t\t\tintervals [1]:\n',
                              '\t\t\t\txmin = 0.00\n',
                              '\t\t\t\txmax = ' + first_max + '\n',
@@ -83,34 +91,45 @@ for file in files:
             if index <= line_ignore:
                 continue
             # If vowel
-            elif re.search(r'text = ', value) and not re.search(consonant, value[12:-2]):
-                try:
-                    mod = 4
-                    vv_unit = value[12:-2]
-                    while re.search(consonant, phone_tier[index+mod][12:-2]):
-                        vv_unit += phone_tier[index + mod][12:-2]
-                        mod += 4
-                    if mod / 4 == 1:
-                        xmax = phone_tier[index+2][11:-1]
-                    elif mod / 4 == 2:
-                        xmax = phone_tier[index+6][11:-1]
-                    elif mod / 4 == 3:
-                        xmax = phone_tier[index+10][11:-1]
-                    vv_tier = vv_tier + ['\t\t\tintervals [1]:\n',
-                                         '\t\t\t\txmin = ' + phone_tier[index - 2][11:-1] + '\n',
-                                         '\t\t\t\txmax = ' + xmax + '\n',
-                                         '\t\t\t\ttext = "' + vv_unit + '"\n']
-                except IndexError:
-                    xmax = phone_tier[4][9:-1]
-                    vv_tier = vv_tier + ['\t\t\tintervals [1]:\n',
-                                         '\t\t\t\txmin = ' + phone_tier[index - 2][11:-1] + '\n',
-                                         '\t\t\t\txmax = ' + xmax + '\n',
-                                         '\t\t\t\ttext = "sil"\n']
+            elif re.search(r'text = ', value):
+                segment = re.search(text, value).group()[1:-1]
+                if not re.search(consonant, segment):
+                    try:
+                        mod = 4
+                        unit_add = re.search(text, phone_tier[index + mod]).group()[1:-1]
+                        while re.search(consonant, unit_add):
+                            segment += unit_add
+                            mod += 4
+                            unit_add = re.search(text, phone_tier[index + mod]).group()[1:-1]
+                        if mod / 4 == 2:
+                            xmax = re.search(time, phone_tier[index + 6]).group()
+                        elif mod / 4 == 3:
+                            xmax = re.search(time, phone_tier[index + 10]).group()
+                        elif mod / 4 == 4:
+                            xmax = re.search(time, phone_tier[index + 14]).group()
+                        elif mod / 4 == 5:
+                            xmax = re.search(time, phone_tier[index + 18]).group()
+                        else:
+                            xmax = re.search(time, phone_tier[index + 2]).group()
+
+                        xmin = re.search(time, phone_tier[index-2]).group()
+
+                        vv_tier = vv_tier + ['\t\t\tintervals [1]:\n',
+                                             '\t\t\t\txmin = ' + xmin + '\n',
+                                             '\t\t\t\txmax = ' + xmax + '\n',
+                                             '\t\t\t\ttext = "' + segment + '"\n']
+                    except IndexError:
+                        xmax = re.search(time, phone_tier[4]).group()
+                        xmin = re.search(time, phone_tier[index - 2]).group()
+                        vv_tier = vv_tier + ['\t\t\tintervals [1]:\n',
+                                             '\t\t\t\txmin = ' + xmin + '\n',
+                                             '\t\t\t\txmax = ' + xmax + '\n',
+                                             '\t\t\t\ttext = "sil"\n']
 
         tg[6] = 'size = 3\n'
 
         # Update the number of the tier
-        vv_tier[0] = '\titem [3]:\n'
+        vv_tier[0] = '\titem [2]:\n'
 
         # Update the name of the tier
         vv_tier[2] = '\t\tname = "syllables"\n'
@@ -128,7 +147,10 @@ for file in files:
             if re.search(r'intervals: size =', line):
                 vv_tier[index] = '\t\tintervals: size = ' + str(size) + '\n'
 
-        final_tier = tg + vv_tier
+        tg[upper_limit] = '\titem [3]:\n'
+
+        final_tier = tg[:upper_limit] + vv_tier + tg[upper_limit:]
+
         final_tier = ''.join(final_tier)
 
         new_tg = open(f'{folder}{file}', 'w')
