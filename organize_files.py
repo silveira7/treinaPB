696cf2561a03a5084cf817bf9f4f43f64f55b822
Gustavo Silveira <silveira@tuta.io>
2021-03-22 12:05:53 -0300

    Rewrite organize_files with much better code

--- organize_files.py
+++ organize_files.py
@@ -1,38 +1,23 @@
-#!/usr/bin/env python3
-
 """This module moves the files of each speaker to a separated folder"""
 
-import os
-import re
-import sys
+from pathlib import Path
 
-folder = sys.argv[1]
 
-os.chdir(folder)
+def organize(folder):
+    p = Path(folder)
 
-files = sorted(os.listdir(folder))
+    p_intervals = p / 'intervals'
+    p_final = p / 'final'
 
-speakers = list()
-for file in files:
-    if re.search(r'\.', file):
-        print(file)
-        speakers.append(file.split(sep='_')[2])
+    p_intervals.mkdir(exist_ok=True)
+    p_final.mkdir(exist_ok=True)
 
-speakers = set(speakers)
-print(speakers)
-
-try:
-    os.makedirs('final')
-except FileExistsError:
-    pass
-
-for speaker in speakers:
-    try:
-        os.makedirs(f'final/{speaker}')
-    except FileExistsError:
-        pass
-
-for speaker in speakers:
-    for file in files:
-        if re.search(speaker, file) and re.search('merged', file):
-            os.rename(file, f'final/{speaker}/{file}')
+    for path in p.iterdir():
+        if path.is_file():
+            if path.match('*merged.TextGrid'):
+                speaker = path.stem.split(sep='_')[2]
+                p_speaker = p_final / f'{speaker}'
+                p_speaker.mkdir(exist_ok=True)
+                path.rename(p_speaker / path.name)
+            else:
+                path.rename(p_intervals / path.name)
